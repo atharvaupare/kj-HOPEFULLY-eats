2082a9d4007ea53c68839fa61d8dd5ecd51f73f1
atharvaupare <atharvaupare5@gmail.com>
2024-06-17 23:37:48 +0530

    feat: sessioncard using slug

--- src/api/sessioncard/controllers/sessioncard.js
+++ src/api/sessioncard/controllers/sessioncard.js
@@ -1,9 +1,41 @@
-'use strict';
+// 'use strict';
+
+// /**
+//  * sessioncard controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::sessioncard.sessioncard');
+
+"use strict";
 
 /**
  * sessioncard controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::sessioncard.sessioncard');
+module.exports = createCoreController(
+  "api::sessioncard.sessioncard",
+  ({ strapi }) => ({
+    async findBySlug(ctx) {
+      const { slug } = ctx.params;
+
+      const entity = await strapi.db
+        .query("api::sessioncard.sessioncard")
+        .findOne({
+          where: { slug },
+          populate: true,
+        });
+
+      if (!entity) {
+        return ctx.notFound();
+      }
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
--- src/api/sessioncard/routes/sessioncard.js
+++ src/api/sessioncard/routes/sessioncard.js
@@ -1,9 +1,26 @@
-'use strict';
+// 'use strict';
 
-/**
- * sessioncard router
- */
+// /**
+//  * sessioncard router
+//  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+// const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::sessioncard.sessioncard');
+// module.exports = createCoreRouter('api::sessioncard.sessioncard');
+
+"use strict";
+
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/sessioncards/:slug",
+      handler: "sessioncard.findBySlug",
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+  ],
+};
