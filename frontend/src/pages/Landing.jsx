9b19513bf5f4e89646849cda89c882dcebc43e7c
Anuj <mailanujhere@gmail.com>
2024-10-01 12:17:35 +0530

    landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,30 +1,28 @@
-import React, { useContext } from "react";
-import authContext from "../context/authContext";
-import Button from "@mui/material/Button";
-
 const Landing = () => {
-  const { user } = useContext(authContext);
-
   return (
-    <>
-      <div className="text-3xl bg-red-300 w-full p-4">
-        {user && user.length > 0 ? (
-          <ul>
-            {user.map((item, index) => (
-              <li key={index} className="p-4 m-2 bg-white border rounded shadow">
-                <div className="font-bold">Username: {item.username}</div>
-                <div className="text-gray-700">Address: {item.address}</div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No users available</p>
-        )}
+    <div className="w-screen h-[100vh] bg-gradient-to-b from-[#462b9c] to-[#644ab5] flex justify-center items-center">
+      <div className="h-[70vh] md:h-[60vh] w-[90vw] flex flex-col justify-between items-center">
+        <div className="size-[240px] rounded-full overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+          <img
+            src="https://img.freepik.com/free-photo/delicious-burger-with-fresh-ingredients_23-2150857908.jpg"
+            alt="Delicious Burger"
+            className="w-full h-full object-cover object-[center_30%]"
+          />
+        </div>
+        <div className="flex flex-col gap-5">
+          <div className="text-white text-4xl font-sans text-center font-semibold">
+            <h1>KJ</h1>
+            <h1>Eats.</h1>
+          </div>
+          <button
+            className="rounded-md bg-white text-[#462b9c] text-xl py-2 px-5 cursor-pointer hover:scale-105 hover:bg-[#462b9c] hover:text-white"
+            type="button"
+          >
+            Get Started
+          </button>
+        </div>
       </div>
-      {/* <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button> */}
-    </>
+    </div>
   );
 };
 
